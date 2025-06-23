6a9e51cb5bb13cc879810866a8a4864402c6b867
julietjaoko <jaokojuliet@gmail.com>
2025-06-24 00:04:08 +0300

    Initial commit: portfolio layout and pages

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./*.{html,js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        plum : '#8E2975',
+      },
+    },
+  },
+  plugins: [],
+}
+
